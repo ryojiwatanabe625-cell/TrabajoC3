9ef5d45d5a3d962a3cb48e0a57c568a8ae9efe6c
MaXafy <ryojiwatanabe625@gmail.com>
2026-04-08 16:54:24 -0500

    nuevo cambio en waaa.py

--- waaa.py
+++ waaa.py
@@ -1,1 +1,1 @@
-print("se clono el repsitorio compartido https://github.com/ryojiwatanabe625-cell/TrabajoC3.git")
+print("se clono repsitorio compartido https://github.com/ryojiwatanabe625-cell/TrabajoC3.git")
